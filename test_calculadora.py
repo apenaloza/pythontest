5c732c5f9b7204d98ba5d7d77322c0605ace4957
Alejandro Peñaloza Otarola <apenaloza@MacBook-Air-de-Alejandro.local>
2025-09-04 16:19:24 -0400

    fix

--- test_calculadora.py
+++ test_calculadora.py
@@ -2,7 +2,7 @@ from calculadora import sumar, restar
 
 def test_sumar():
     assert sumar(2,3) ==5
-    assert restar(-1,1) == 0
+    assert sumar(-1,1) == 0
 
 def test_restar():
     assert restar(5,3) ==2
